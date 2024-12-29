c528b00c67bf08892fb141bbeb36af149baf71de
pelonidas <jannahalka@icloud.com>
2024-12-29 15:10:22 +0100

    Add new plugins for completion and filesystem navigation

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
       "williamboman/mason.nvim",
       "williamboman/mason-lspconfig.nvim",
+      "saghen/blink.cmp"
     },
     config = function()
       require("mason").setup()
@@ -11,8 +12,9 @@ return {
         ensure_installed = { "lua_ls", "pyright", "gopls", "tsserver" },
       }
 
-      require 'lspconfig'.lua_ls.setup {}
-      require 'lspconfig'.pyright.setup {}
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require 'lspconfig'.lua_ls.setup { capabilities = capabilities }
+      require 'lspconfig'.pyright.setup { capabilities = capabilities }
     end,
   }
 }
